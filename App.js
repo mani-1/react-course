de46f7e12d712ee7ac047c3aab66c9e98e640116
mani <manikandan.tms88@gmail.com>
2024-01-06 12:17:38 +0530

    initial react app start using cdn

---
+++ App.js
@@ -0,0 +1,24 @@
+/**
+ *  
+ *  <div id="parent">
+ *    <div id="child">
+ *        <h1>Im h1 tag</h1>
+ *        <h2>Im h2 tag</h2>
+ *    </div>
+ *  </div>
+ * 
+ */
+
+const parent = React.createElement('div', { id: 'parent' },
+  React.createElement('div', { id: 'child' },
+    [React.createElement('h1', {}, 'Im h1 tag'),
+    React.createElement('h2', {}, 'Im h2 tag')]
+  )
+)
+
+
+const heading = React.createElement('h1', { id: 'heading', className: 'title' }, 'Hello world')
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(heading)
